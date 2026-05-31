45a96cfd0e29353be5a5ca3477b8ee0e75538688
Gundu-Nikhitha15 <nikhithagundu1504@gmail.com>
2026-05-31 15:39:34 +0530

    Upload Take Up Order project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TakeUpOrderModal from "../components/TakeUpOrderModal";
+
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <TakeUpOrderModal />
+    </main>
+  );
+}
